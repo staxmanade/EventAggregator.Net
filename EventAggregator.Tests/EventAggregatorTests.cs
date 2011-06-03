d880673dc01a4bbdba3bf2fee17ab402d83ddc6e
Jason Jarrett <jason@elegantcode.com>
2011-06-03 05:54:59 -0700

    added extension that allows delegate listener to subscribe

--- EventAggregator.Tests/EventAggregatorTests.cs
+++ EventAggregator.Tests/EventAggregatorTests.cs
@@ -68,7 +68,35 @@ namespace EventAggregator.Net.Tests
 
 			warningWasCalled.ShouldBeTrue();
 		}
-	}
+
+
+
+        [Fact]
+        public void Can_use_delegate_to_subscribe_to_message()
+        {
+            var eventAggregator = new EventAggregationManager();
+            SomeMessage messageTrapped = null;
+
+            eventAggregator.AddListenerAction<SomeMessage>(msg => { messageTrapped = msg; });
+            eventAggregator.SendMessage<SomeMessage>();
+
+            messageTrapped.ShouldNotBeNull();
+        }
+
+
+        [Fact]
+        public void Can_use_unsubscribe_from_delegate_handler()
+        {
+            var eventAggregator = new EventAggregationManager();
+            SomeMessage messageTrapped = null;
+
+            var disposable = eventAggregator.AddListenerAction<SomeMessage>(msg => { messageTrapped = msg; });
+            disposable.Dispose();
+            eventAggregator.SendMessage<SomeMessage>();
+
+            messageTrapped.ShouldBeNull();
+        }
+    }
 
 
 	public class SomeMessage { }
---
+++ EventAggregator/EventAggregatorExtensions.cs
@@ -0,0 +1,51 @@
+using System;
+
+namespace EventAggregator.Net
+{
+    public static class EventAggregatorExtensions
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Reliability", "CA2000:Dispose objects before losing scope")]
+        public static IDisposable AddListenerAction<T>(this IEventSubscriptionManager eventAggregator, Action<T> listener)
+        {
+            if (eventAggregator == null) throw new ArgumentNullException("eventAggregator");
+            if (listener == null) throw new ArgumentNullException("listener");
+
+            var delegateListener = new DelegateListener<T>(listener, eventAggregator);
+            eventAggregator.AddListener(delegateListener);
+
+            return delegateListener;
+        }
+    }
+
+    public class DelegateListener<T> : IListener<T>, IDisposable
+    {
+        private readonly Action<T> _listener;
+        private readonly IEventSubscriptionManager _eventSubscriptionManager;
+
+        public DelegateListener(Action<T> listener, IEventSubscriptionManager eventSubscriptionManager)
+        {
+            _listener = listener;
+            _eventSubscriptionManager = eventSubscriptionManager;
+        }
+
+        public void Handle(T message)
+        {
+            _listener(message);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _eventSubscriptionManager.RemoveListener(this);
+            }
+        }
+    }
+
+}
