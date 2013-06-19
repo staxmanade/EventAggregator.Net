08f837ee6391ff07236b3cda0bd495921320be4b
Joseph Plant <git@JosephPlant.com>
2013-06-19 12:34:11 +1000

    Fixed Issue #5 where message type is also checked

--- EventAggregator.Tests/SendMessageWithBaseTypeTests.cs
+++ EventAggregator.Tests/SendMessageWithBaseTypeTests.cs
@@ -11,7 +11,7 @@ namespace EventAggregatorNet.Tests
 	    [Fact]
 		public void Should_be_able_to_send_using_base_type()
 		{
-			var someMessageHandler = new MyClassHandler();
+            var someMessageHandler = new NewMessageHandler();
             var eventAggregator = new EventAggregator();
 			eventAggregator.AddListener(someMessageHandler);
 
@@ -20,7 +20,52 @@ namespace EventAggregatorNet.Tests
 			someMessageHandler.WasHandled.ShouldBeTrue();
 		}
 
-        public class MyClassHandler : IListener<NewMessage>
+        [Fact]
+        public void Should_be_able_to_send_to_both_using_base_type()
+        {
+            var newMessageHandler = new NewMessageHandler();
+            var baseMessageHandler = new BaseMessageHandler();
+            var eventAggregator = new EventAggregator();
+            eventAggregator.AddListener(newMessageHandler);
+            eventAggregator.AddListener(baseMessageHandler);
+
+            eventAggregator.SendMessage<BaseMessage>(new NewMessage());
+
+            baseMessageHandler.WasHandled.ShouldBeTrue();
+            newMessageHandler.WasHandled.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void Should_match_same_type_only()
+        {
+            var newMessageHandler = new NewMessageHandler();
+            var baseMessageHandler = new BaseMessageHandler();
+            var eventAggregator = new EventAggregator();
+            eventAggregator.AddListener(newMessageHandler);
+            eventAggregator.AddListener(baseMessageHandler);
+
+            eventAggregator.SendMessage(new BaseMessage());
+
+            baseMessageHandler.WasHandled.ShouldBeTrue();
+            newMessageHandler.WasHandled.ShouldBeFalse();
+        }
+
+        [Fact]
+        public void Should_be_able_to_send_using_object_type()
+        {
+            var newMessageHandler = new NewMessageHandler();
+            var baseMessageHandler = new BaseMessageHandler();
+            var eventAggregator = new EventAggregator();
+            eventAggregator.AddListener(newMessageHandler);
+            eventAggregator.AddListener(baseMessageHandler);
+
+            eventAggregator.SendMessage<object>(new BaseMessage());
+
+            baseMessageHandler.WasHandled.ShouldBeTrue();
+            newMessageHandler.WasHandled.ShouldBeFalse();
+        }
+
+        public class NewMessageHandler : IListener<NewMessage>
 		{
             public void Handle(NewMessage message)
 			{
@@ -29,5 +74,15 @@ namespace EventAggregatorNet.Tests
 
 			public bool WasHandled { get; set; }
 		}
+
+        public class BaseMessageHandler : IListener<BaseMessage>
+        {
+            public void Handle(BaseMessage message)
+            {
+                WasHandled = true;
+            }
+
+            public bool WasHandled { get; set; }
+        }
 	}
 }
